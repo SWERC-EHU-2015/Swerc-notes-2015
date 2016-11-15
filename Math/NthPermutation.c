bd6cb7a8ac1e97d0f4cc016f8f7770b3e9220b6c
iomist <iomist@openmailbox.org>
2016-11-15 16:54:27 +0100

    Rename IthPermutation to NthPermutation

--- Math/NthPermutation.c
+++ Math/NthPermutation.c
@@ -1,27 +1,27 @@
-// n The number of entries
-// i The index of the permutation
+// e The number of entries
+// n The index of the permutation
 int fact[MAX];  // MAX >= n
 int perm[MAX];  // MAX >= n
-void ithPermutation(const int n, int i) {
+void nthPermutation(const int e, int n) {
    int j, k = 0;
 
    // compute factorial numbers
    fact[k] = 1;
-   while (++k < n)
+   while (++k < e)
       fact[k] = fact[k - 1] * k;
 
    // compute factorial code
    for (k = 0; k < n; ++k) {
       perm[k] = i / fact[n - 1 - k];
-      i = i % fact[n - 1 - k];
+      n = n % fact[e - 1 - k];
    }
 
    // readjust values to obtain the permutation
    // start from the end and check if preceding values are lower
-   for (k = n - 1; k > 0; --k)
+   for (k = e - 1; k > 0; --k)
       for (j = k - 1; j >= 0; --j)
          if (perm[j] <= perm[k])
             perm[k]++;
 
-   // perm[0..n] contains the ith permutation
+   // perm[0..e] contains the nth permutation
 }

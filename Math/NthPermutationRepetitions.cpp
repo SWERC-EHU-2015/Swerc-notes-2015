379880098c62d6be039cc4934a368b5793fdac59
iomist <iomist@openmailbox.org>
2016-11-15 17:02:49 +0100

    Use global typedef when possible

--- Math/NthPermutationRepetitions.cpp
+++ Math/NthPermutationRepetitions.cpp
@@ -1,14 +1,13 @@
 typedef map<char,int> mci;
-typedef long long ll;
-ll factorial(ll i) {
+L factorial(L i) {
 	if (i <= 1) return 1;
 
-	ll totala = 2;
-	for (ll j = 3; j <= i; ++j)
+	L totala = 2;
+	for (L j = 3; j <= i; ++j)
 		totala *= j;
 	return totala;
 }
-string nthPermutationRepetitions(string input, ll n) {
+string nthPermutationRepetitions(string input, L n) {
 	mci mapa;
 	int input_len = input.length();
 	for (int i = 0; i < input_len; ++i) {
@@ -20,12 +19,12 @@ string nthPermutationRepetitions(string input, ll n) {
 
 	string buffer;
 	buffer.resize(input_len);
-	ll totala = 0;
+	L totala = 0;
 	for (int i = 0; i < input_len; ++i) {
 		for (mci::iterator elem = mapa.begin(); elem != mapa.end(); ++elem) {
 			if (elem->second > 0) {
 				elem->second--;
-				ll perm = factorial(input_len - i -1);
+				L perm = factorial(input_len - i -1);
 				for (mci::iterator c = mapa.begin(); c != mapa.end(); ++c)
 					perm /= factorial(c->second);
 

bad0f325d94119699e487616e71524e24e9192d3
fushuan <enk0pm@gmail.com>
2017-11-23 23:49:46 +0100

    added last position retrieval of the texts found alongside the instances

--- StringProcessing/AhoCorasick.py
+++ StringProcessing/AhoCorasick.py
@@ -15,6 +15,7 @@ class AhoCorasick:
         self.needle_root = Node(None)
         self.needle_root.fail_goto = self.needle_root
         self.n_needles = 0
+        self.needles = list()
 
     def set_fails(self):
         to_process = deque()
@@ -41,8 +42,8 @@ class AhoCorasick:
                 to_process.append(child)
 
     def add_needle(self, needle):
-        self.n_needles += 1
         cur_node = self.needle_root
+
         for char in needle:
             for child in cur_node.children.values():
                 if child.val == char:
@@ -52,17 +53,32 @@ class AhoCorasick:
                 new = Node(char, parent=cur_node)
                 cur_node.children[char] = new
                 cur_node = new
-        cur_node.pattern_ids.add((self.n_needles, needle))
+        if needle not in cur_node.pattern_ids:
+            cur_node.pattern_ids.add(self.n_needles)
+            self.needles.append(needle)
+            self.n_needles += 1
 
     def search_in(self, string):
         cur_node = self.needle_root
-        found_needles = set()
-        for char in string:
-            found_needles |= cur_node.pattern_ids
+        found_needles = []
+        found_table = [set() for _ in range(len(string))]
 
-            jump = cur_node.children.get(char, -1)
+        i = 0
+        while i < len(string):
+
+            jump = cur_node.children.get(string[i], -1)
             if not jump == -1:
                 cur_node = jump
+                for id_ in cur_node.pattern_ids:
+                    found_table[i].add(id_)
+                    found_needles.append(self.needles[id_])
+                i += 1
             else:
                 cur_node = cur_node.fail_goto
-        return found_needles
+                for id_ in cur_node.pattern_ids:
+                    found_table[i - 1].add(id_)
+                    found_needles.append(self.needles[id_])
+                if cur_node == self.needle_root:
+                    i += 1
+
+        return found_needles, found_table
